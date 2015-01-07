824a36f2c5872999f1396799c0c6c129778e5f3b
hulukaihuaba <15700086047@163.com>
2015-01-07 09:01:56 +0800

    2015/1/7

---
+++ New/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long a = 10L;
+		long b=(long) 9.998;
+		System.out.println("a-b=" + (a-b));
+	}
+
+}
